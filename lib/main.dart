820b76153f367e49342d0fdca246b70240e44c03
MostafaRadian <mostafa.batesta@gmail.com>
2023-08-18 06:49:24 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:to_do_list/screens/to_do.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(debugShowCheckedModeBanner: false, home: Todo());
+  }
+}
---
+++ lib/screens/to_do.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class Todo extends StatelessWidget {
+  const Todo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xffe9edf3),
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: const Color(0xfffefefe),
+        elevation: 0,
+        leading: const Icon(
+          Icons.menu,
+          color: Colors.black,
+        ),
+        title: const Text(
+          "My To-Do",
+          style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.w900,
+              fontFamily: "times new roman"),
+        ),
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.add,
+                color: Colors.black,
+              ))
+        ],
+      ),
+    );
+  }
+}
